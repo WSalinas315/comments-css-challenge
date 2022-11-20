c7ad498c45070723b273b2b6bf7309446ff745bc
Billy Salinas <wsalinas315@gmail.com>
2022-11-19 21:43:19 -0600

    color changes working on individual elements

--- client.js
+++ client.js
@@ -1,9 +1,25 @@
-$(document).ready(onReady);
+// $(document).ready(onReady);
 
-// on document load
-function onReady(){
-    // setup click listeners
-    //setupListeners();
-    // call get
-    //getTasks();
-}
+// // on document load
+// function onReady(){
+//     $('.upvote').on('mouseover', upVoteMouse);
+//     $('.voting').on('mouseout','.upvote', upVoteOut);
+//     // $('.upvote').on('mouseover', upVoteMouse);
+//     // $('.upvote').on('mouseout', downVoteMouse);
+// }
+
+// function upVoteMouse(){
+//     elementIs = $(this);
+//     //console.log('Upvote element is:', elementIs);
+//     $(this).empty();
+//     $(this).append(`
+//         <img src="./images/icon-plus-hover.svg" />
+//     `);
+// }
+
+// function upVoteOut(){
+//     $(this).empty();
+//     $(this).append(`
+//         <img src="./images/icon-plus.svg" />
+//     `);
+// }
